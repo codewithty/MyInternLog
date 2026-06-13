c8de03ecb0a3876054050bd5cb1ae6f4efcad907
codewithty <tyler88967@gmail.com>
2026-06-13 16:12:16 -0400

    Initial MyInternLog project setup

---
+++ MyInternLog/MyInternLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyInternLogApp.swift
+//  MyInternLog
+//
+//  Created by Tyler on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct MyInternLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
